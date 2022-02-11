608cd70163f87d06e82525ecfda52f3e872ae828
David <david@Davids-MacBook-Air.local>
2022-02-11 15:10:23 +0100

    Added game and player entities boilerplate CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GameModule } from './game/game.module';
+import { PlayerModule } from './player/player.module';
 
 @Module({
-  imports: [],
+  imports: [GameModule, PlayerModule],
   controllers: [AppController],
   providers: [AppService],
 })
