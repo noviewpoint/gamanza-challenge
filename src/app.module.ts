0697871ea72cd1902446e06e2ffd9a59537a49e1
David <david@Davids-MacBook-Air.local>
2022-02-14 18:20:22 +0100

    Changed response caching to use redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,19 @@ import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
 import { PlayerModule } from './player/player.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import type { RedisClientOptions } from 'redis';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
-    CacheModule.register({
+    CacheModule.register<RedisClientOptions>({
       isGlobal: true,
       ttl: 30, // seconds
+      store: redisStore,
+      socket: {
+        host: 'localhost',
+        port: 6379,
+      },
     }),
     GameModule,
     PlayerModule,
