fe4853b063c43cb5c0879fceca80a8f5f8ab7703
David <david@Davids-MacBook-Air.local>
2022-02-14 17:49:58 +0100

    Added HTTP GET global response caching on controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module, CacheInterceptor } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
 import { PlayerModule } from './player/player.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [GameModule, PlayerModule],
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 30, // seconds
+    }),
+    GameModule,
+    PlayerModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    // Only GET endpoints are cached.
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+    AppService,
+  ],
 })
 export class AppModule {}
