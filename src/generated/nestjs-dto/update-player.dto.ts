7ee6d6fe6ccd5409265cfd3fa1d4cdbe2d65ab55
David <david@Davids-MacBook-Air.local>
2022-02-15 08:30:31 +0100

    prisma schema changes

--- src/generated/nestjs-dto/update-player.dto.ts
+++ src/generated/nestjs-dto/update-player.dto.ts
@@ -1,12 +1,8 @@
-import { Prisma } from '@prisma/client';
-
 export class UpdatePlayerDto {
-  joinedOn?: Date;
   firstName?: string;
   lastName?: string;
   middleName?: string;
   bornDate?: Date;
   email?: string;
   activated?: boolean;
-  credits?: Prisma.Decimal;
 }
